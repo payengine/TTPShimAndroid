681f4792bc5660b7882baaa6c265c319d093464b
Tri Nguyen <tri@platformfactory.io>
2025-07-25 12:09:56 +0700

    banner name

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.platformfactory.pesoftpos"
         minSdk = 26
         targetSdk = 35
-        versionCode = 18
-        versionName = "1.8"
+        versionCode = 19
+        versionName = "1.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
